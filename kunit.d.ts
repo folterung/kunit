189993478d24891cdaf4b034e8c94c706a58fa96
Kevin Warnock <kevin.warnock@capitalone.com>
2017-03-12 16:41:36 -0400

    Added declaration file.

---
+++ kunit.d.ts
@@ -0,0 +1,18 @@
+declare function Mock(): void;
+declare function Fixture(): void;
+declare function Instance(): void;
+
+declare function Before(): void;
+declare function BeforeAsync(): void;
+
+declare function BeforeEach(): void;
+declare function BeforeEachAsync(): void;
+
+declare function Test(): void;
+declare function TestAsync(): void;
+
+declare function AfterEach(): void;
+declare function AfterEachAsync(): void;
+
+declare function After(): void;
+declare function AfterAsync(): void;
