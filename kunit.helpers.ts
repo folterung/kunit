8f69727df0ac2e94ff522a446ab58ba55b2cf859
Kevin Warnock <kevin.warnock@capitalone.com>
2017-03-13 06:15:48 -0400

    deepCopy now normalizes the prototype of the object as well.

--- kunit.helpers.ts
+++ kunit.helpers.ts
@@ -2,6 +2,10 @@ import { KUnit } from './kunit';
 import { kunitEnums } from './kunit.enums';
 import { nativeConstants } from './native.constants';
 
+interface CopyObject {
+  [key: string]: any;
+}
+
 export const kunitMetadata = 'kunit:metadata';
 
 export function updateMetadata(key: number, value: Object) {
@@ -41,47 +45,61 @@ export function generateMock(target: any): any {
 
   // Create new instance with the correct arguments
   const targetInstance = new (Function.prototype.bind.apply(targetType, paramTypes));
+  const copy: CopyObject = {};
 
-  return createShell(targetInstance);
-}
+  Object.setPrototypeOf(copy, Object.getPrototypeOf(targetInstance));
 
-function createShell(obj: any): any {
-  let type: string;
-  let shell: any = Object.create(obj);
-
-  Object.keys(obj)
-    .forEach((key: string) => {
-      type = getType(obj[key]);
-
-      switch (type) {
-        case nativeConstants.string:
-          shell[key] = '';
-          break;
-        case nativeConstants.number:
-          shell[key] = 0;
-          break;
-        case nativeConstants.null:
-          shell[key] = null;
-          break;
-        case nativeConstants.undefined:
-          shell[key] = undefined;
-          break;
-        case nativeConstants.array:
-          shell[key] = [];
-          break;
-        case nativeConstants.object: {
-          shell[key] = createShell(obj[key]);
-          break;
-        }
-      }
+  // TODO: Extract logic to normalize prototype and object members.
+  Object.keys(Object.getPrototypeOf(copy))
+    .forEach((newKey: string) => {
+      copy[newKey] = _setDefaultValues(copy, newKey, getType(copy[newKey]));
     });
 
-  return shell;
+  return _deepCopy(copy, targetInstance);
+
+  function _deepCopy(shell: any, obj: any): any {
+    // Set all me`mbers to a default value.
+    Object.keys(obj)
+      .forEach((key: string) => {
+        const type = getType(obj[key]);
+
+        shell[key] = _setDefaultValues(obj, key, type);
+      });
+
+    return shell;
+  }
+
+  function _setDefaultValues(originalObj: any, key: string, type: string) {
+    switch (type) {
+      case nativeConstants.string: return '';
+      case nativeConstants.number: return 0;
+      case nativeConstants.null: return null;
+      case nativeConstants.undefined: return undefined;
+      case nativeConstants.function: return noop;
+      case nativeConstants.array: return [];
+      case nativeConstants.object:
+        const newCopy: CopyObject = {};
+        Object.setPrototypeOf(newCopy, Object.getPrototypeOf(originalObj[key]));
+
+        Object.keys(Object.getPrototypeOf(newCopy))
+          .forEach((newKey: string) => {
+            newCopy[newKey] = _setDefaultValues(newCopy, newKey, getType(newCopy[newKey]));
+          });
+
+        return _deepCopy(newCopy, originalObj[key]);
+    }
+  }
 }
 
-function getType(t: Object): string {
+
+
+function getType(t: any): string {
   const typeString = Object.prototype.toString.call(t);
   const trueType = (/\[object\s(\w*)\]/.exec(typeString) as RegExpMatchArray)[1];
 
   return trueType.toLowerCase();
+}
+
+function noop() {
+  /* noop function */
 }
